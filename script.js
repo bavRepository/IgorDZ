0a39c755c295defb8521cc29b7688b0e204be48b
a.barai <a.barai6003@gmail.com>
2023-06-26 12:08:48 +0300

    completedCreateMethod

--- script.js
+++ script.js
@@ -12,80 +12,48 @@ let noticeValue;
 let shiftX;
 let shiftY;
 
-function addNoticeToLocalStorage(wrap, notice) {
 
-	const elemsData = {
-		id: `id${wrap.getAttribute('data-index')}`,
-		x: `${wrap.style.left}`,
-		y: `${wrap.style.top}`,
-		z: `${wrap.style.zIndex}`,
-		message: `${notice?.value ?? ""}`,
-	};
 
-	localStorage.setItem(`${elemsData.id}`, JSON.stringify(elemsData));
-}
+class NoticeService {
 
-function getLocalNoticeInfo(data, i) {
-	let storageValue;
-	const localItems = JSON.parse(localStorage.getItem(`id${i+1}`));
+	#localStorageKey = 'notice';
 
-	switch (data) {
-		case 'x': storageValue = localItems.x;
-			break;
-		case 'y': storageValue = localItems.y;
-			break;
-		case 'id': storageValue = localItems.id;
-			break;
-		case 'z': storageValue = localItems.z;
-			break;
-		case 'message': storageValue = localItems.message;
-			break;
-		default: console.error("Вы запросили несуществующую инструкцию у LocalStorage. Таких данных нет");
+	delete(noticeId) {
+		localStorage.removeItem(this.#localStorageKey);
 	}
 
-	return storageValue;
-}
-
-function startApplicationWork() {
-	if (localStorage.length > 0) {
-		for (let i = 0; i < localStorage.length; i++) {
-
-			elemCoordX = getLocalNoticeInfo('x', i);
-			elemCoordY = getLocalNoticeInfo('y', i);
-			zIndex = getLocalNoticeInfo('z', i);
-			noticeValue = getLocalNoticeInfo('message', i);
-			addNewNotice();
-		}
+	create(noticeObj) {
+		localStorage.setItem(this.#localStorageKey, JSON.stringify(noticeObj));
 	}
+
+	read(noticeId) {
+		// let storageValue;
+		const localItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
+
+
+
+	}
+
+	update(data) {
+		localStorage.setItem(this.#localStorageKey);
+	}
+
+
 }
-
-function clearZIndex() {
-	const notices = document.querySelectorAll('.notice');
-	const wrappers = document.querySelectorAll('.noticeWrapper');
-	wrappers.forEach((wrap, i) => {
-		wrap.style.zIndex = 'auto';
-		addNoticeToLocalStorage(wrap, notices[i]);
-	});
-}
-
-
-
-function getNoticeFromLocalStorage(elem) {
-	localStorage.removeItem(`id${elem.getAttribute('data-index')}`);
-}
-
 function setUpNoticeSettings() {
 
+	
 
 	const notices = document.querySelectorAll('.notice');
 	const wrappers = document.querySelectorAll('.noticeWrapper');
 
 	wrappers.forEach((elem) => {
+	
 		const notice = elem.querySelector('.notice');
 		const delButton = elem.querySelector('button');
-
-		addNoticeToLocalStorage(elem, notice);
-
+	
+		noticeObj.create(getNoticeObjData(elem, notice));
+		
 		// add Bold border if element is last cicked
 		if (Number(elem.style.zIndex) === 1) {
 			notice.style.border = noticeClickedStyleBorder;
@@ -113,13 +81,13 @@ function setUpNoticeSettings() {
 			document.addEventListener('mousemove', withMouseMoove);
 
 			elem.addEventListener("mouseup", function () {
-				addNoticeToLocalStorage(elem, notice);
+				noticeObj.create(getNoticeObjData(elem, notice));
 
 				document.removeEventListener('mousemove', withMouseMoove);
 			});
 		});
 		elem.addEventListener('input', () => {
-			addNoticeToLocalStorage(elem, notice);
+			noticeObj.create(getNoticeObjData(elem, notice));
 		});
 		delButton.addEventListener('click', () => {
 			getNoticeFromLocalStorage(elem);
@@ -129,6 +97,104 @@ function setUpNoticeSettings() {
 
 
 }
+
+
+
+
+const noticeObj = new NoticeService();
+
+function getNoticeObjData(elem, notice) {
+
+	return {
+		id: `id${elem.getAttribute('data-index')}`,
+		x: `${elem.style.left}`,
+		y: `${elem.style.top}`,
+		z: `${elem.style.zIndex}`,
+		message: `${notice?.value ?? ""}`,
+	};
+	
+}
+
+
+
+function addNoticeToLocalStorage(wrap, notice) {
+
+	const elemsData = {
+		id: `id${wrap.getAttribute('data-index')}`,
+		x: `${wrap.style.left}`,
+		y: `${wrap.style.top}`,
+		z: `${wrap.style.zIndex}`,
+		message: `${notice?.value ?? ""}`,
+	};
+
+	localStorage.setItem(`${elemsData.id}`, JSON.stringify(elemsData));
+}
+
+function getLocalNoticeInfo(data, i) {
+	let storageValue;
+	const localItems = JSON.parse(localStorage.getItem(`id${i + 1}`));
+
+	switch (data) {
+		case 'x': storageValue = localItems.x;
+			break;
+		case 'y': storageValue = localItems.y;
+			break;
+		case 'id': storageValue = localItems.id;
+			break;
+		case 'z': storageValue = localItems.z;
+			break;
+		case 'message': storageValue = localItems.message;
+			break;
+		default: console.error("Вы запросили несуществующую инструкцию у LocalStorage. Таких данных нет");
+	}
+
+	return storageValue;
+}
+
+// function startApplicationWork() {
+// 	if (localStorage.length > 0) {
+// 		for (let i = 0; i < localStorage.length; i++) {
+
+// 			elemCoordX = getLocalNoticeInfo('x', i);
+// 			elemCoordY = getLocalNoticeInfo('y', i);
+// 			zIndex = getLocalNoticeInfo('z', i);
+// 			noticeValue = getLocalNoticeInfo('message', i);
+// 			addNewNotice();
+// 		}
+// 	}
+// }
+
+
+function startApplicationWork() {
+	if (localStorage.length > 0) {
+		for (let i = 0; i < localStorage.length; i++) {
+
+			elemCoordX = notice.read('x', i);
+			elemCoordY = getLocalNoticeInfo('y', i);
+			zIndex = getLocalNoticeInfo('z', i);
+			noticeValue = getLocalNoticeInfo('message', i);
+			addNewNotice();
+		}
+	}
+}
+
+
+
+function clearZIndex() {
+	const notices = document.querySelectorAll('.notice');
+	const wrappers = document.querySelectorAll('.noticeWrapper');
+	wrappers.forEach((wrap, i) => {
+		wrap.style.zIndex = 'auto';
+		// noticeObj.create(wrap, notices[i]);
+	});
+}
+
+
+
+function getNoticeFromLocalStorage(elem) {
+	localStorage.removeItem(`id${elem.getAttribute('data-index')}`);
+}
+
 
 function createNoticeWrapper() {
 
