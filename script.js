1419a38e55a0076db801b7471ade20e95073b5af
a.barai <a.barai6003@gmail.com>
2023-10-16 10:27:03 +0300

    addFuncAddButton

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+class NoticeManager {
+	create() {}
+}
+
+function setAddButtonListener() {
+	const addButton = document.querySelector('.addNotice');
+	addButton.addEventListener('click', function () {
+		nm.create();
+	});
+}
+const nm = new NoticeManager();
+setAddButtonListener();
