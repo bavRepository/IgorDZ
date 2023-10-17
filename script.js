13da19955f8d1312a5595bff0cebc6b898e0fdce
a.barai <a.barai6003@gmail.com>
2023-10-17 10:03:23 +0300

    addMoveListener

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let shiftX;
 let shiftY;
+let noticeID = 0;
 const container = document.querySelector('.container');
 const wrappers = document.querySelectorAll('.noticeWrapper');
 
@@ -9,14 +10,19 @@ class NoticeManager {
 	create() {
 		const noticeWrapper = document.createElement('div');
 		const notice = document.createElement('textarea');
-
 		noticeWrapper.classList.add('noticeWrapper');
+		noticeWrapper.setAttribute('data-id', `${++noticeID}`);
 		notice.classList.add('notice');
 		const delButton = createDelButton();
 		noticeWrapper.append(notice);
 		noticeWrapper.append(delButton);
 		container.append(noticeWrapper);
 	}
+}
+
+class LocalStorageManager {
+	#localStorageKey = 'notice';
+	create() {}
 }
 
 function createDelButton() {
@@ -37,10 +43,27 @@ function setNoticeListeners() {
 	container.addEventListener('click', function (e) {
 		const target = e.target;
 		if (target.classList == 'delBtn') {
-			alert('Е бой!');
+		}
+	});
+	container.addEventListener('mousedown', function (e) {
+		const target = e.target;
+		if (target.classList == 'delBtn' || target.classList == 'notice') {
+			console.log('DA');
+			shiftX =
+				e.clientX -
+				target.closest('.noticeWrapper').getBoundingClientRect().left;
+			shiftY =
+				e.clientY -
+				target.closest('.noticeWrapper').getBoundingClientRect().top;
+
+			document.addEventListener('mousemove', function (e) {
+				target.closest('.noticeWrapper').style.left = e.pageX - shiftX + 'px';
+				target.closest('.noticeWrapper').style.top = e.pageY - shiftY + 'px';
+			});
 		}
 	});
 }
 const nm = new NoticeManager();
 setAddButtonListener();
 setNoticeListeners();
+// setDragAndDropListeners();
