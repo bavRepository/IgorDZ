367f63ddb1f19c7b749260b6798e44c7d32b7d7e
a.barai <a.barai6003@gmail.com>
2023-10-17 11:59:05 +0300

    addNoticeMoveAction2.0

--- script.js
+++ script.js
@@ -6,18 +6,43 @@ let noticeID = 0;
 const container = document.querySelector('.container');
 const wrappers = document.querySelectorAll('.noticeWrapper');
 
-class NoticeManager {
-	create() {
-		const noticeWrapper = document.createElement('div');
-		const notice = document.createElement('textarea');
-		noticeWrapper.classList.add('noticeWrapper');
-		noticeWrapper.setAttribute('data-id', `${++noticeID}`);
-		notice.classList.add('notice');
-		const delButton = createDelButton();
-		noticeWrapper.append(notice);
-		noticeWrapper.append(delButton);
-		container.append(noticeWrapper);
-	}
+function creatingNotice() {
+	const noticeWrapper = document.createElement('div');
+	const notice = document.createElement('textarea');
+	noticeWrapper.classList.add('noticeWrapper');
+	noticeWrapper.setAttribute('data-id', `${++noticeID}`);
+	notice.classList.add('notice');
+	const delButton = createDelButton();
+	noticeWrapper.append(notice);
+	noticeWrapper.append(delButton);
+	container.append(noticeWrapper);
+
+	noticeWrapper.addEventListener('click', function (e) {
+		const target = e.target;
+
+		if (target.classList == 'notice') {
+			target.style.borderWidth = '3px';
+		}
+
+		if (target.classList == 'delBtn') {
+			noticeWrapper.remove();
+		}
+	});
+
+	noticeWrapper.addEventListener('mousedown', function (e) {
+		shiftX = e.clientX - noticeWrapper.getBoundingClientRect().left;
+		shiftY = e.clientY - noticeWrapper.getBoundingClientRect().top;
+		console.log(shiftX + ' ' + shiftY);
+		document.addEventListener('mousemove', mMove);
+		function mMove(e) {
+			noticeWrapper.style.transform = 'none';
+			noticeWrapper.style.left = e.pageX - shiftX + 'px';
+			noticeWrapper.style.top = e.pageY - shiftY + 'px';
+		}
+		noticeWrapper.addEventListener('mouseup', function () {
+			document.removeEventListener('mousemove', mMove);
+		});
+	});
 }
 
 class LocalStorageManager {
@@ -32,38 +57,11 @@ function createDelButton() {
 	return delButton;
 }
 
-function setAddButtonListener() {
+function addNoticeButtonFn() {
 	const addButton = document.querySelector('.addNotice');
 	addButton.addEventListener('click', function () {
-		nm.create();
+		creatingNotice();
 	});
 }
 
-function setNoticeListeners() {
-	container.addEventListener('click', function (e) {
-		const target = e.target;
-		if (target.classList == 'delBtn') {
-		}
-	});
-	container.addEventListener('mousedown', function (e) {
-		const target = e.target;
-		if (target.classList == 'delBtn' || target.classList == 'notice') {
-			console.log('DA');
-			shiftX =
-				e.clientX -
-				target.closest('.noticeWrapper').getBoundingClientRect().left;
-			shiftY =
-				e.clientY -
-				target.closest('.noticeWrapper').getBoundingClientRect().top;
-
-			document.addEventListener('mousemove', function (e) {
-				target.closest('.noticeWrapper').style.left = e.pageX - shiftX + 'px';
-				target.closest('.noticeWrapper').style.top = e.pageY - shiftY + 'px';
-			});
-		}
-	});
-}
-const nm = new NoticeManager();
-setAddButtonListener();
-setNoticeListeners();
-// setDragAndDropListeners();
+addNoticeButtonFn();
