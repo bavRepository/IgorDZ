906b8f788e61b307fda65ab7929f68d1f639364b
a.barai <a.barai6003@gmail.com>
2023-12-12 15:50:16 +0200

    fix function getDragAble

--- src/js/script.js
+++ src/js/script.js
@@ -50,10 +50,16 @@ class LocalStorageManager {
     const noticeFromLocal =
       localStorage.getItem(`${this.#localStorageKey}`) ?? "[]";
 
-    const withoutDeleted = JSON.parse(noticeFromLocal).filter((item) => {
-      return noticeDataObj.timeCreating !== item.elemId;
+    const localAnswer = JSON.parse(noticeFromLocal);
+    const withoutDeleted = localAnswer.filter((item) => {
+      console.log(typeof noticeDataObj.timeCreating);
+      console.log(typeof item.elemId);
+      return toString(noticeDataObj.timeCreating) !== toString(item.elemId);
     });
 
+    withoutDeleted.forEach(item => {
+      console.log(item); 
+  });
     localStorage.setItem(
       `${this.#localStorageKey}`,
       JSON.stringify(withoutDeleted)
@@ -143,7 +149,7 @@ class Notice {
 class Desktop {
   #startPositionLeft = "40%";
   #startPositionTop = "40%";
-  #startZIndex = "auto";
+  #startZIndex = "10";
   #currentNoticeAndDragAbleElemData;
   #storageService;
   #arrayWrapAndNoteInformation = [];
@@ -152,7 +158,7 @@ class Desktop {
     this.#storageService = serviceObj;
     const elem = document.querySelector(".addNotice");
     elem.addEventListener("click", () => {
-      desktop.createNewNotice();
+      this.createNewNotice();
     });
   }
 
@@ -161,16 +167,55 @@ class Desktop {
   }
 
    getDragAble(
-    wrapperLeft = this.#startPositionLeft,
-    wrapperTop = this.#startPositionTop,
-    wrapperZIndex = this.#startZIndex
+     elemInd
   ) {
+
+    const wrapperLeft = this.#arrayWrapAndNoteInformation[elemInd].left ?? this.#startPositionLeft,
+    wrapperTop = this.#arrayWrapAndNoteInformation[elemInd].top ?? this.#startPositionTop,
+    wrapperZIndex = this.#arrayWrapAndNoteInformation[elemInd].zIndex ?? this.#startZIndex,
+    timeCreating = this.#arrayWrapAndNoteInformation[elemInd].elemId ?? 0;
+
+    // wrapperLeft = this.#startPositionLeft,
+    // wrapperTop = this.#startPositionTop,
+    // wrapperZIndex = this.#startZIndex,
+    // timeCreating = 0
+
     const dragAbleElem = document.createElement("div");
+    dragAbleElem.setAttribute('data-time', `${timeCreating}`);
     dragAbleElem.classList.add("wrapperNotice");
     dragAbleElem.style.position = "absolute";
     dragAbleElem.style.left = wrapperLeft;
     dragAbleElem.style.top = wrapperTop;
     dragAbleElem.style.zIndex = wrapperZIndex;
+
+
+      dragAbleElem.addEventListener('mousedown', (e) => {
+    	// this.resetNoticesStyle(this.#currentNoticeAndDragAbleElemData);
+
+    	const shiftX = e.clientX - dragAbleElem.getBoundingClientRect().left;
+    	const shiftY = e.clientY - dragAbleElem.getBoundingClientRect().top;
+
+    	function mMove(e) {
+      
+    	desktop.#arrayWrapAndNoteInformation[desktop.#arrayWrapAndNoteInformation.length - 1].left = e.pageX - shiftX + 'px';
+    	desktop.#arrayWrapAndNoteInformation[desktop.#arrayWrapAndNoteInformation.length - 1].top = e.pageY - shiftY + 'px';
+
+    				dragAbleElem.style.left = desktop.#arrayWrapAndNoteInformation[desktop.#arrayWrapAndNoteInformation.length - 1].left;
+    				dragAbleElem.style.top = desktop.#arrayWrapAndNoteInformation[desktop.#arrayWrapAndNoteInformation.length - 1].top;
+
+    	}
+    	document.addEventListener('mousemove', mMove);
+    	dragAbleElem.addEventListener('mouseup', function () {
+
+    		document.removeEventListener('mousemove', mMove);
+    		// console.log(desktop.#storageService + " desktopLcalService");
+    		// console.log(desktop.#arrayWrapAndNoteInformation + " desktopNashArray");
+    		desktop.#storageService.create(desktop.#arrayWrapAndNoteInformation);
+    	});
+    });
+
+
+
     return dragAbleElem;
   }
 
@@ -193,9 +238,11 @@ class Desktop {
 
       this.#notice.forEach((n, i) => {
         const wrapperElem = this.getDragAble(
-          this.#arrayWrapAndNoteInformation[i].left,
-          this.#arrayWrapAndNoteInformation[i].top,
-          this.#arrayWrapAndNoteInformation[i].zIndex
+          i
+          // this.#arrayWrapAndNoteInformation[i].left,
+          // this.#arrayWrapAndNoteInformation[i].top,
+          // this.#arrayWrapAndNoteInformation[i].zIndex,
+          // this.#arrayWrapAndNoteInformation[i].elemId
         );
         const htmlNoticeElem = n.getHtmlNotice();
         wrapperElem.append(htmlNoticeElem);
@@ -211,8 +258,13 @@ class Desktop {
         item.onInput = () => {
           this.storageService.update(item);
         };
+        // item.onMove
       });
     }
+  }
+
+  updateNotice() {
+
   }
 
   createNewNotice() {
